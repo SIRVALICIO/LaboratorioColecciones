c32e7cec8468b4eb7316d316ed6e9c670af947f1
valquero779 <andresserrano2002@hotmail.com>
2019-04-16 17:19:24 -0500

    primer commit

---
+++ Libraries/custom_functions/temperature_methods.py
@@ -0,0 +1,21 @@
+def Departamentprom(a,b,c,d,e,f,g,h,i,j,k,m):
+
+    return (a+b+c+d+f+g+h+i+j+k+m+e)/12
+
+def DeapartamentMax(a,b,c,d,e,f,g,h,i,j,k,m):
+    list=[a,b,c,d,e,f,g,h,i,j,k,m]
+    Result=max(list)
+    return Result
+def DepartamentMaximus(a,b,c):
+    list=[a,b,c]
+    Result=max(list)
+    return Result
+def DepartamentDes(a,b,c,d,f,g,h,i,j,k,m):
+ import  statistics
+ list=[a,b,c,d,f,g,h,i,j,k,m]
+ Result=statistics.stdev(list)
+ return Result
+
+
+
+
