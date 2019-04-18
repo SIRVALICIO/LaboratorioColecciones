0fdd61abe11f7942ac03ce2ea1ec41035bd4583c
valquero779 <andresserrano2002@hotmail.com>
2019-04-17 20:08:10 -0500

    Pruebas para el video

--- Libraries/test/test_temperature_methods.py
+++ Libraries/test/test_temperature_methods.py
@@ -5,22 +5,22 @@ from custom_functions import temperature_methods
 class TestCollectionMethods(unittest.TestCase):
 
     def test_Departamentprom(self):
-        Total= temperature_methods.Departamentprom(12,25,14,28,36,27,39,40,24,14,13,25)
-        self.assertEqual(Total, 3.75)
+        Total= temperature_methods.Departamentprom(12,12,12,12,12,12,12,12,12,12,12,12)
+        self.assertEqual(Total, 13)
 
     def test_DepartamentMax(self):
-        Total = temperature_methods.DepartamentMax(12, 25, 14, 28, 36, 27, 39, 40, 24, 14, 13, 25)
-        self.assertEqual(Total, 3.75)
+        Total = temperature_methods.DepartamentMax(450,12,12,12,12,12,12,12,12,12,12,12)
+        self.assertEqual(Total, 450)
 
 
 
     def test_DepartamentMaximus(self):
         Total = temperature_methods.DepartamentMaximus(12, 25, 14,)
-        self.assertEqual(Total, 3.75)
+        self.assertEqual(Total, 25)
 
     def test_DepartamentDes(self):
-        Total = temperature_methods.DepartamentDes(12, 25, 14, 28, 36, 27, 39, 40, 24, 14, 13, 25)
-        self.assertEqual(Total, 3.75)
+        Total = temperature_methods.DepartamentDes(12, 12,12,12,12,12,13,12,12,12,12,12)
+        self.assertEqual(Total, 0)
 
 
 if __name__ == '__main__':
