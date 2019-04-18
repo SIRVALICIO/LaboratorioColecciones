1adb7a0ff24f71cfb85f22fd3a4c31cfb69f4413
valquero779 <andresserrano2002@hotmail.com>
2019-04-17 21:13:52 -0500

    Nuevo

--- Libraries/main/laboratory.py
+++ Libraries/main/laboratory.py
@@ -9,7 +9,7 @@ def climas(a):
     narino={"Enero":24,"Febrero":25,"Marzo":24,"Abril":23,"Mayo":22,"Junio":25,"Julio":26,"Agosto":22,"Septiembre":27,"Octubre":25,"Novimbre":24,"Diciembre":22}
 
     if a=="Guajira":
-        print("-El departamento de eleccion es: ", a, "\n -Acontinuacion se le pedira opciones para ingresar: ","\n  -Si escribe el nombre del mes, podra saber ese mes en especifico","\n  -Si escribe TODOS, se imprimiran todos los meses","\n  -Escriba MAX para saber el de mayor temperatura","\n  -Y por ultimo PROM para saber el promedio del departamento")
+        print("-El departamento de eleccion es: ", a, "\n -Acontinuacion se le pedira opciones para ingresar: ","\n  -Si escribe el nombre del mes, podra saber ese mes en especifico","\n  -Si escribe TODOS, se imprimiran todos los meses","\n  -Escriba MAX para saber el de mayor temperatura","\n  -Y por ultimo PROM para saber el promedio del departamento","\n  -para saber la desviacion estander de esta region coloque ESTANDAR")
         b = input("\n-Opcion o mes: ")
         if b=="TODOS":
             for keys, values in guajira.items():
@@ -32,7 +32,7 @@ def climas(a):
 
 
     if a == "Santander":
-        print("-El departamento de eleccion es: ", a, "\n -Acontinuacion se le pedira opciones para ingresar: ","\n  -Si escribe el nombre del mes, podra saber ese mes en especifico","\n  -Si escribe TODOS, se imprimiran todos los meses","\n  -Escriba MAX para saber el de mayor temperatura","\n  -Y por ultimo PROM para saber el promedio del departamento")
+        print("-El departamento de eleccion es: ", a, "\n -Acontinuacion se le pedira opciones para ingresar: ","\n  -Si escribe el nombre del mes, podra saber ese mes en especifico","\n  -Si escribe TODOS, se imprimiran todos los meses","\n  -Escriba MAX para saber el de mayor temperatura","\n  -Y por ultimo PROM para saber el promedio del departamento","\n  -para saber la desviacion estander de esta region coloque ESTANDAR")
         b = input("\n-Opcion o mes: ")
         if b == "TODOS":
             for keys, values in santander.items():
@@ -53,7 +53,7 @@ def climas(a):
 
 
     if a == "Nariño":
-        print("-El departamento de eleccion es: ", a, "\n -Acontinuacion se le pedira opciones para ingresar: ","\n  -Si escribe el nombre del mes, podra saber ese mes en especifico","\n  -Si escribe TODOS, se imprimiran todos los meses","\n  -Escriba MAX para saber el de mayor temperatura","\n  -Y por ultimo PROM para saber el promedio del departamento")
+        print("-El departamento de eleccion es: ", a, "\n -Acontinuacion se le pedira opciones para ingresar: ","\n  -Si escribe el nombre del mes, podra saber ese mes en especifico","\n  -Si escribe TODOS, se imprimiran todos los meses","\n  -Escriba MAX para saber el de mayor temperatura","\n  -Y por ultimo PROM para saber el promedio del departamento","\n  -para saber la desviacion estander de esta region coloque ESTANDAR")
         b = input("\n-Opcion o mes: ")
         if b == "TODOS":
             for keys, values in narino.items():
@@ -72,36 +72,45 @@ def climas(a):
                         print("La temperatura en: ", b, "es de: ", narino[b],"°C")
 
     if a=="Nacional":
-        print("-El departamento de eleccion es: ", a, "\n -Acontinuacion se le pedira opciones para ingresar:","\n  -Quiere saber la  desviacion estandar, entonces coloque ESTANDAR, y se le dara esta informcaion ","\n  -Si quere saber cual de los 3 departamentos anteriores mencionados es el mayor y su promedio, ingrese MAX","\n  -Si quiere saber le mes mas caliente de todos los departamentos y en que mes, ingrese ULTIMATE")
+        print("-El departamento de eleccion es: ", a, "\n -Acontinuacion se le pedira opciones para ingresar:","\n  -Quiere saber la  desviacion estandar, entonces coloque ESTANDAR, y se le dara esta informcaion ","\n  -Si quere saber cual de los 3 departamentos anteriores mencionados es el mayor y su promedio, ingrese MAX","\n  -Si quiere saber le mes mas caliente de todos los departamentos y en que mes, ingrese ULTIMATE","\n  -Si quiere saber el promedio de las temperaturas mas calientes de los 3 departamentos escribir HEATBLAST")
         b=input("\nOpcion a elegir: ")
         if b=="ESTANDAR":
             print("la desviacion estandar de Guajira es: ",statistics.stdev(guajira.values()),"°C")
             print("la desviacion estandar de Nariño es: ",statistics.stdev(narino.values()),"°C")
             print("la desviacion estandar de la Santander es: ",statistics.stdev(santander.values()),"°C")
-        else:
-            if b=="MAX":
-                promG=sum(guajira.values())/12
-                promS=sum(santander.values()) / 12
-                promN=sum(narino.values()) / 12
-                promL=[promN,promG,promS]
 
-                print("El promedio de La guajira es de: ",promG)
-                print("El promedio de La Santander es de: ", promS)
-                print("El promedio de La Nariño es de: ",promN)
-                print("El mayor de todos las temperaturas es el de: ",max(promL))
-            else:
-                if b=="ULTIMATE":
-                    ultimateG=max(guajira.values())
-                    ultimateS=max(santander.values())
-                    ultimateN=max(narino.values())
-                    if ultimateG>ultimateS and ultimateG>ultimateN:
-                        print("En la Guajira se registro la temperatura maxima en: ",max(guajira, key=guajira.get),"con: ", max(narino.values()),"°C")
-                    else:
-                        if ultimateS>ultimateN and ultimateS>ultimateG:
-                            print("En el Santander se registro la temperatura maxima en: ", max(santander, key=santander.get),"con: ", max(narino.values()),"°C")
+        else:
+              if b=="HEATBLAST":
+                  MaxGuajira= max(guajira.values())
+                  MaxSantander=max(santander.values())
+                  MaxNarino=max(narino.values())
+                  Maxprom= MaxGuajira+MaxNarino+MaxSantander
+                  print("El promedio de los meses mas calientes es de: ",Maxprom,"°C")
+              else:
+                if b=="MAX":
+                    promG=sum(guajira.values())/12
+                    promS=sum(santander.values()) / 12
+                    promN=sum(narino.values()) / 12
+                    promL=[promN,promG,promS]
+
+                    print("El promedio de La guajira es de: ",promG)
+                    print("El promedio de La Santander es de: ", promS)
+                    print("El promedio de La Nariño es de: ",promN)
+                    print("El mayor de todos las temperaturas es el de: ",max(promL))
+                else:
+                    if b=="ULTIMATE":
+                        ultimateG=max(guajira.values())
+                        ultimateS=max(santander.values())
+                        ultimateN=max(narino.values())
+                        if ultimateG>ultimateS and ultimateG>ultimateN:
+                            print("En la Guajira se registro la temperatura maxima en: ",max(guajira, key=guajira.get),"con: ", max(narino.values()),"°C")
                         else:
-                            if ultimateN>ultimateG and ultimateN>ultimateS:
-                                print("En el Nariño se registro la temperatura maxima en: ",max(narino, key=narino.get),"con: ", max(narino.values()),"°C")
+                            if ultimateS>ultimateN and ultimateS>ultimateG:
+                                print("En el Santander se registro la temperatura maxima en: ", max(santander, key=santander.get),"con: ", max(narino.values()),"°C")
+                            else:
+                                if ultimateN>ultimateG and ultimateN>ultimateS:
+                                    print("En el Nariño se registro la temperatura maxima en: ",max(narino, key=narino.get),"con: ", max(narino.values()),"°C")
+
     b = input("-Quiere continuar?, Si/No: ")
     while b!="No":
          a = input("-Ingrese un departamento entre\n-Santander\n-Nariño\n-Guajira\n-O elegirlo a nivel nacional\n:")
